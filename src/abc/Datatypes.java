c8ba3dfc1df23499553bd850a5b6f057541bc2cb
Surajgaigol <surajgaigol@gmail.com>
2022-11-12 18:53:45 +0530

    commited1

---
+++ src/abc/Datatypes.java
@@ -0,0 +1,35 @@
+package abc;
+
+public class Datatypes 
+{
+      public static void main(String[] args)
+   
+   {
+    	  String name="SURAJ";
+    	  String surname="Gaigol";
+    	  String address= "JALGAON JAMOD";
+    	  String university="SANTA GADGEBABA AMRAVATI UNIVERSITY";
+    	  String bloodgroup="AB+";
+	      int passout=2018;
+	      char division='a';
+	      long pincode=443402;
+	      float cgpa=7.83f;
+	      byte a=111;
+	      short b=3333;
+	      double c=11.23456;
+	      
+	      
+	      System.out.println(name+"_______"+surname);
+	      System.out.println(address+"___________"+university);
+	      System.out.println(passout);
+	      System.out.println(bloodgroup);
+	      System.out.println(division);
+	      System.out.println(pincode);
+	      System.out.println(cgpa);
+	      System.out.println(a);
+	      System.out.println(b);
+	      System.out.println(c);
+	      
+	      
+}
+}
---
+++ src/abc/Stringpr.java
@@ -0,0 +1,20 @@
+package abc;
+
+public class Stringpr {
+	
+	public static void main(String[] args)
+	{
+		String name="SURAJ";                       //String literal
+		String s= new String("GAIGOL");            //String new key
+		System.out.println(name);
+		System.out.println(s);
+		
+		String a="java";
+		String b="Developer";
+		System.out.println(a.equals(b));
+		System.out.println(a.charAt(0));
+		System.out.println(a.length());
+		System.out.println(b.replace('b', 'l'));
+		System.out.println(a.toUpperCase());
+	}
+}
